be87e247bbcb8814c46df3910f8ea7ddfdc26f88
Irshad <irshadali644@gmail.com>
2023-12-19 18:38:53 +0530

    project updated

--- src/Components/Projects/mockData.js
+++ src/Components/Projects/mockData.js
@@ -4,32 +4,25 @@ import rentPayment from "../../Assets/rentPayment.PNG";
 import project4 from "../../Assets/project4.PNG";
 
 export const projectData = [
-  // {
-  //   id: 1,
-  //   image: portfolio,
-  //   title: "My Portfolio",
-  //   githubCodeUrl: "https://github.com/nasir8405/my-portfolio",
-  //   demoUrl: "https://portfolio-nasir-khan.netlify.app/",
-  // },
   {
     id: 2,
     image: rentPayment,
     title: "Rent Payment",
-    githubCodeUrl: "https://github.com/nasir8405",
+    githubCodeUrl: "",
     demoUrl: "https://rentpayment.in/",
   },
   {
     id: 3,
     image: project4,
     title: "E-commerce",
-    githubCodeUrl: "https://github.com/nasir8405/e-commerce-website",
-    demoUrl: "https://electronic-commerce-website.netlify.app/",
+    githubCodeUrl: "",
+    demoUrl: "",
   },
   {
     id: 4,
     image: crudApp,
     title: "CRUD App",
-    githubCodeUrl: "https://github.com/nasir8405/crud-operation-redux",
-    demoUrl: "https://nasir-khan-crud-app.netlify.app/",
+    githubCodeUrl: "",
+    demoUrl: "",
   },
 ];
