28bd48fdc896f01fcf0c88fdbe2a1b9cdee6c143
Irshad <irshadali644@gmail.com>
2023-12-20 15:37:19 +0530

    ui update

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -5,11 +5,13 @@ export const Header = () => {
     <section id="projects">
       <div className="container">
         <div className="header-container">
+         
           <h3>Hi there! I'm Md. Irshad Ansari</h3>
+          <h1>Front-End Developer</h1>
           <p>
             I Love Solving Problems. Strong in design and integration with
-            intuitive problem-solving skills. Proficient in JAVASCRIPT,
-            REACT.JS, HTML and CSS Passionate about implementing and launching
+            intuitive problem-solving skills. Proficient in HTML, Css,Bootstrap,Jquery,JavaScript
+            REACT.JS, Passionate about implementing and launching
             new projects.
           </p>
         </div>
